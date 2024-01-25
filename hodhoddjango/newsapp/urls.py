a7b6ca7445d48a1e34be1b2497d91b3a1b3cae1b
mahdiahmadi87 <mahdi.ahmadi.1387@gmail.com>
2024-01-25 15:24:33 +0330

    add newsapp to django

--- hodhoddjango/newsapp/urls.py
+++ hodhoddjango/newsapp/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path("", include("newsapp.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.homePage, name="homePage"),
 ]

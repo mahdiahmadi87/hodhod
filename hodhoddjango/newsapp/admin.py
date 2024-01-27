ec290c1b57a795f4851f339a10f3d0e0ab1eae43
mahdiahmadi87 <mahdi.ahmadi.1387@gmail.com>
2024-01-27 19:04:13 +0330

    update model fields with database

--- hodhoddjango/newsapp/admin.py
+++ hodhoddjango/newsapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import News, Category
+from .models import News, Tag, Topic
 
 # Register your models here.
 
 admin.site.register(News)
-admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Topic)
--- hodhoddjango/newsapp/models.py
+++ hodhoddjango/newsapp/models.py
@@ -2,20 +2,31 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
+class Tag(models.Model):
     title = models.CharField(max_length=300)
 
     def __str__(self):
         return self.title
 
+
+class Topic(models.Model):
+    title = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
+
+
 class News(models.Model):
-    category = models.ManyToManyField(Category)
+    id = models.CharField(max_length=15, primary_key=True)
     title = models.CharField(max_length=500)
-    detail = models.TextField()
-    add_time = models.DateTimeField(auto_now_add=True)
+    abstract = models.TextField(null=True)
+    tags = models.ManyToManyField(Tag)
+    topics = models.ManyToManyField(Topic)
+    link = models.CharField(max_length=1000, null=True)
+    published = models.CharField(max_length=20, null=True)
 
 
 
     def __str__(self):
-        
         return self.title
+    

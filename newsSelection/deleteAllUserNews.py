a90d6b36e7622870c68bbfef42591b7d5c77e887
mahdiahmadi87 <mahdi.ahmadi.1387@gmail.com>
2024-06-20 15:42:58 +0330

    update a few small things ...

--- newsSelection/deleteAllUserNews.py
+++ newsSelection/deleteAllUserNews.py
@@ -2,10 +2,11 @@ import sqlite3
 
 conn = sqlite3.connect('./../userNews.db')
 
-usernames = list(set(conn.execute("SELECT username from Users")))
+usernames = list(set(conn.execute("SELECT username from Viewed")))
 
 for row in usernames:
-    conn.execute(f"DELETE from Users where username = '{row[0]}';")
+    conn.execute(f"DELETE from Viewed where username = '{row[0]}';")
+    conn.execute(f"DELETE from Interests where username = '{row[0]}';")
 
 
 conn.commit()

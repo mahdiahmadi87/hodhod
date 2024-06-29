4f3cc3b3c3cdae2de4c6b98f052dd244dc8fd8d2
mahdiahmadi87 <mahdi.ahmadi.1387@gmail.com>
2024-06-29 18:28:50 +0330

    add favicon.ico

--- hodhoddjango/hodhoddjango/urls.py
+++ hodhoddjango/hodhoddjango/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 from django.urls import path, include
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path("", include("newsapp.urls")),
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')), 
+    path('favicon.ico', favicon_view),
 ]
 

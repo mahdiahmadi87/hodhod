530c08ad5df7c4ed98564f0c0b9b4f6b37b38a1b
mahdiahmadi87 <mahdi.ahmadi.1387@gmail.com>
2024-08-12 18:25:57 +0330

    update web addres

--- crawlers/sync.py
+++ crawlers/sync.py
@@ -2,5 +2,5 @@ import requests
 import time
 
 while True:
-    print(requests.get("http://51.58.137.82:11111/dbToDjango/"))
+    print(requests.get("http://51.68.137.82:11111/dbToDjango/"))
     time.sleep(5)

430d8e25494828fbdf20f321b288e737c3652bec
mahdiahmadi87 <mahdi.ahmadi.1387@gmail.com>
2024-08-12 18:23:49 +0330

    print requests status in sync.py

--- crawlers/sync.py
+++ crawlers/sync.py
@@ -2,5 +2,5 @@ import requests
 import time
 
 while True:
-    requests.get("http://51.58.137.82:11111/dbToDjango/")
+    print(requests.get("http://51.58.137.82:11111/dbToDjango/"))
     time.sleep(5)
